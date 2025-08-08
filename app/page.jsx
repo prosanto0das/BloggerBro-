8723ea4c3e184e4a1194649e015833727ed3e784
prosanto0das <prosanto0das23@gmail.com>
2025-08-08 12:15:29 +0600

    footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 'use client';
 import BlogList from '@/Components/BlogList';
+import Footer from '@/Components/Footer';
 import Header from '@/components/Header';
 export default function Home() {
   return (
     <> 
         <Header/>
         <BlogList/>
+        <Footer/>
     </>
   ) 
 }
